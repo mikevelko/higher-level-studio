4b028e1efd3b436dfb10ec60b5cc0d8c586852d9
mikevelko <mikeveko@gmail.com>
2025-10-19 19:29:21 +0200

    wip

--- packages/ui/components/sections/header-mobile/index.tsx
+++ packages/ui/components/sections/header-mobile/index.tsx
@@ -64,12 +64,7 @@ export function HeaderMobile({ links, image, className }: IMobileHeaderProps) {
               </Button>
               <div className="flex flex-col items-center p-4 pt-20">
                 {links.map((link, i) => (
-                  <Link
-                    key={i}
-                    {...link}
-                    className="mt-8"
-                    onClick={toggleMenu}
-                  />
+                  <Link key={i} {...link} className="mt-8" />
                 ))}
               </div>
             </div>
